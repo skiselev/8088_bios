2d6d32692f46df67e75323164a8ffa16f9a01ca9
Sergey Kiselev <skiselev@gmail.com>
2017-12-14 17:21:12 -0800

    setup: move user input related functions to setup.inc
    Signed-off-by: Sergey Kiselev <skiselev@gmail.com>

--- bios.asm
+++ bios.asm
@@ -247,7 +247,9 @@ mouse_data	equ	28h	; 8 bytes - mouse data buffer
 ; Includes
 ;-------------------------------------------------------------------------
 %include	"messages.inc"		; POST messages
+%ifndef MACHINE_XT			; No space in XT ROM for font
 %include	"fnt80-FF.inc"		; font for graphics modes
+%endif ; MACHINE_XT
 ;%include	"inttrace.inc"		; XXX
 %ifdef AT_RTC
 %include	"rtc.inc"		; RTC and CMOS read / write functions
@@ -423,181 +425,6 @@ ipl:
 	mov	ah,00h
 	int	16h
 	jmp	.retry
-
-;=========================================================================
-; get_line - read an ASCIIZ string from the console
-; Input:
-;	ES:DI - pointer to the buffer
-;	AX - max string length
-; Output:
-;	AX - string length
-; Notes:
-;	Buffer must have size of max string length + 1 to accomodate 00h
-;-------------------------------------------------------------------------
-get_line:
-	push	bx
-	push	dx
-	push	si
-	push	di
-
-	cld
-	mov	si,di
-	add	si,ax			; SI = last character position
-	xor	dx,dx			; DX = 0 - string length
-
-.read_char_loop:
-	mov	ah,00h			; read character from keyboard
-	int	16h
-
-	cmp	al,08h			; <Backspace> key
-	jz	.backspace
-
-	cmp	al,0Dh			; <Enter> key
-	jz	.enter
-
-	cmp	al,20h			; below printable ASCII code?
-	jb	.error_input
-
-	cmp	al,7Eh			; above printable ASCII code?
-	ja	.error_input
-
-	cmp	di,si			; end of buffer reached?
-	jae	.error_input
-
-	stosb				; store character in the buffer
-	inc	dx			; increment strng length
-
-	mov	ah,0Eh			; teletype output (echo)
-	mov	bx,0007h
-	int	10h
-
-	jmp	.read_char_loop
-
-.backspace:
-	or	dx,dx			; empty string?
-	jz	.error_input
-
-	dec	di			; move pointer back
-	dec	dx			; decrement string size
-
-	mov	ax,0E08h		; move the cursor back
-	mov	bx,0007h
-	int	10h
-
-	mov	ax,0E20h		; erase the character under the cursor
-	mov	bx,0007h
-	int	10h
-
-	mov	ax,0E08h		; move the cursor back again
-	mov	bx,0007h
-	int	10h
-
-	jmp	.read_char_loop
-
-.error_input:
-	mov	ax,0E07h		; beep
-	mov	bx,0007h
-	int	10h
-
-	jmp	.read_char_loop
-
-.enter:
-	mov	al,00h			; store 00h at the end of the string
-	stosb
-
-	mov	ax,0E0Dh		; CR
-	mov	bx,0007h
-	int	10h
-
-	mov	ax,0E0Ah		; LF
-	mov	bx,0007h
-	int	10h
-
-	mov	ax,dx			; string length to AX
-
-	pop	di
-	pop	si
-	pop	dx
-	pop	bx
-	ret
-
-;=========================================================================
-; atoi - convert ASCIIZ string to an 16-bit integer number
-; Input:
-;	ES:DI - pointer to string
-; Output:
-;	AX - number
-; 	ES:DI - pointer moved to the position following the number
-;-------------------------------------------------------------------------
-atoi:
-	push	bx
-	push	cx
-	push	dx
-
-	xor	ax,ax			; zero the result
-	mov	bx,10			; multiplier
-
-.atoi_loop:
-    es	mov	cl,byte [di]
-
-	cmp	cl,'0'			; ASCII code below '0'
-	jb	.exit
-
-	cmp	cl,'9'			; ASCII code above '9'
-	ja	.exit
-
-	inc	di			; move to the next character
-
-	sub	cl,'0'			; convert to ASCII to binary
-
-	mul	bx			; DX:AX = AX * 10
-	mov	ch,0
-	add	ax,cx			; AX = AX + CX
-	
-	jmp	.atoi_loop
-
-.exit:
-	pop	dx
-	pop	cx
-	pop	bx
-	ret
-
-;=========================================================================
-; bin_to_bcd - convert binary number to a packed BCD
-; Input:
-;	AX - binary number
-; Output:
-;	AX - packed BCD number
-;-------------------------------------------------------------------------
-bin_to_bcd:
-	push	bx
-	push	cx
-	push	dx
-	push	si
-
-	mov	cl,0			; shift amount
-	xor	si,si			; zero result
-	mov	bx,10			; BX - divisor
-
-.bin_to_bcd_loop:
-	xor	dx,dx			; DX - zero for 32-bit div operand
-	div	bx
-
-	shl	dx,cl			; shift digit to the required position
-	add	cl,4			; calculate next position
-
-	add	si,dx			; add reminder to the result
-	or	ax,ax			; quotient is zero?
-	jnz	.bin_to_bcd_loop
-
-	mov	ax,si			; result to AX
-
-	pop	si
-	pop	dx
-	pop	cx
-	pop	bx
-	ret
-
 
 ;=========================================================================
 ; print - print ASCIIZ string to the console
@@ -1055,7 +882,11 @@ interrupt_table:
 	dw	int_dummy		; INT 1C - User Timer Tick
 	dw	int_1D			; INT 1D - Video Parameters Table
 	dw	int_1E			; INT 1E - Floppy Paameters Table
+%ifndef MACHINE_XT
 	dw	int_1F			; INT 1F - Font For Graphics Mode
+%else ; MACHINE_XT
+	dw	int_ignore
+%endif ; MACHINE_XT
 
 %ifdef SECOND_PIC
 interrupt_table2:
@@ -1682,7 +1513,7 @@ int_19:
 config_table:
 	dw	.size			; bytes 0 and 1: size of the table
 .bytes:
-	db	MODEL_BYTE		; byte 2: model = AT
+	db	MODEL_BYTE		; byte 2: model
 	db	00h			; byte 3: submodel = 0
 	db	00h			; byte 4: release = 0
 %ifdef SECOND_PIC
